a658303d89aabe92f15aa81f85cc7ab4f6ba5635
Sam Siggs <samsiggs00@gmail.com>
2021-10-19 08:12:44 +1300

    Setting up API

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import Trade as Trade
 import requests
 import urllib.parse
 import hashlib
@@ -35,6 +36,12 @@ class KrakenAPI:
     API_BALANCE = '/0/private/Balance'
     API_TRADE_BALANCE = '/0/private/TradeBalance'
     API_OPEN_ORDERS = '/0/private/OpenOrders'
+    API_CLOSED_ORDERS = '/0/private/ClosedOrders'
+    API_QUERY_ORDERS = '/0/private/QueryOrders'
+    API_TRADE_HISTORY = '/0/private/TradesHistory'
+    API_TRADE_INFO = '/0/private/QueryTrades'
+    API_OPEN_POSITIONS = '/0/private/OpenPositions'
+    API_LEDGERS_INFO = '/0/private/Ledgers'
 
     def kraken_request(self, uri_path, data, api_key, api_sec):
         """Attaches auth headers and returns results of a POST request"""
@@ -125,6 +132,113 @@ class KrakenAPI:
             "nonce": str(int(1000 * time.time())),
             "trades": trades
         }, API_KEY, API_SEC)
+
+    def get_closed_orders(self, user_ref: int, start: int, end: int, ofs: int, close_time: str = 'both',
+                          trades: bool = False):
+        """ Retrieve information about orders that have been closed (filled or cancelled).
+            50 results are returned at a time, the most recent by default.
+            :parameter trades: bool - Whether or not to include trades related to position in output. Default: False, Enum: True, False
+            :parameter user_ref: int - Restrict results to given user reference id
+            :parameter start: int - Starting unix timestamp or order tx ID of results (exclusive)
+            :parameter end: int - Ending unix timestamp or order tx ID of results (inclusive)
+            :parameter ofs: int - Result offset for pagination
+            :parameter close_time: str - Which time to use to search. Default: 'both', Enum: 'open', 'close', 'both'
+        :return: Closed Orders
+        """
+        return self.kraken_request(self.API_CLOSED_ORDERS, {
+            "nonce": str(int(1000 * time.time())),
+            "user_ref": user_ref,
+            "trades": trades,
+            "start": start,
+            "end": end,
+            "ofs": ofs,
+            "closetime": close_time
+        }, API_KEY, API_SEC)
+
+    def query_orders_info(self, txid: str, user_ref: int, trades: bool = False):
+        """ Retrieve information about specific orders.
+        :param txid: (required) Comma delimited list of transaction IDs to query info about (20 maximum)
+        :param user_ref: Restrict results to given user reference id
+        :param trades: Whether or not to include trades related to position in output
+        :return:
+        """
+        return self.kraken_request(self.API_QUERY_ORDERS, {
+            "nonce": str(int(1000 * time.time())),
+            "txid": txid,
+            "trades": trades,
+            "userref": user_ref,
+        }, API_KEY, API_SEC)
+
+    def get_trades_history(self, start: int, end: int, ofs: int, trade_type: str = 'all', trades: bool = False):
+        """ Retrieve information about trades/fills. 50 results are returned at a time, the most recent by default.
+            Unless otherwise stated, costs, fees, prices, and volumes are specified with the precision for the
+            asset pair (pair_decimals and lot_decimals), not the individual assets' precision (decimals).
+        :param start: Starting unix timestamp or trade tx ID of results (exclusive)
+        :param end: Ending unix timestamp or trade tx ID of results (inclusive)
+        :param ofs: Result offset for pagination
+        :param trade_type: Whether or not to include trades related to position in output. Default: False
+        :param trades: Type of trade. Default: 'all' Enum: 'all', 'any position', 'closed position', 'closing position', 'no position'
+        :return:
+        """
+        return self.kraken_request(self.API_TRADE_HISTORY, {
+            "nonce": str(int(1000 * time.time())),
+            "trades": trades,
+            "type": trade_type,
+            "start": start,
+            "end": end,
+            "ofs": ofs,
+        }, API_KEY, API_SEC)
+
+    def query_trade_info(self, txid: str, trades: bool = False):
+        """ Retrieve information about specific trades/fills.
+        :param txid: Comma delimited list of transaction IDs to query info about (20 maximum)
+        :param trades: Whether or not to include trades related to position in output. Default: False
+        :return:
+        """
+        self.kraken_request(self.API_TRADE_INFO, {
+            "nonce": str(int(1000 * time.time())),
+            "txid": txid,
+            "trades": trades
+        }, API_KEY, API_SEC)
+
+    def get_open_positions(self, txid: str, consolidation: str, docalcs: bool = False):
+        """ Get information about open margin positions.
+        :param txid: Comma delimited list of txids to limit output to
+        :param consolidation: Consolidate positions by market/pair. Value: 'market'
+        :param docalcs: Whether to include P&L calculations. Default: False
+        :return:
+        """
+        self.kraken_request(self.API_OPEN_POSITIONS, {
+            "nonce": str(int(1000 * time.time())),
+            "txid": txid,
+            "consolidation": consolidation,
+            "docalcs": docalcs
+        }, API_KEY, API_SEC)
+
+    def get_ledgers_info(self, start: int, end: int, ofs: str, asset: str = 'all', asset_class: str = 'currency', ledger_type: str = 'all'):
+        """ Retrieve information about ledger entries. 50 results are returned at a time, the most recent by default.
+        :param start: Starting unix timestamp or ledger ID of results (exclusive)
+        :param end: Ending unix timestamp or ledger ID of results (inclusive)
+        :param ofs: Result offset for pagination
+        :param asset: Comma delimited list of assets to restrict output to. Default: 'all'
+        :param asset_class: Asset class. Default: 'currency'
+        :param ledger_type: Type of ledger to retrieve. Default: 'all' Enum: 'all', 'deposit', 'withdrawal', 'trade', 'margin'
+        :return: Response
+        """
+        return self.kraken_request(self.API_LEDGERS_INFO, {
+            'nonce': str(int(1000 * time.time())),
+            'asset': asset,
+            'aclass': asset_class,
+            'type': ledger_type,
+            'start': start,
+            'end': end,
+            'ofs': ofs,
+        }, API_KEY, API_SEC)
+
+
+
+
+
 
 
 kraken = KrakenAPI()
