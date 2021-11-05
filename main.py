ef702d9d784c5de5ad63a861b1bc02bba0617e2f
Sam Siggs <samsiggs00@gmail.com>
2021-11-05 21:56:49 +1300

    trying to calculate exponential_moving_average

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from KrakenAPI import KrakenAPI
 from datetime import datetime
 from termcolor import colored
+import pandas as pd
+import numpy as np
 
 kraken = KrakenAPI()
 
+
+def exponential_moving_average(values, period, span):
+    data = pd.DataFrame({'period': period, 'price': values})
+    data['mva'] = data['price'].ewm(span=span, adjust=False).mean()
+    newData = data['mva'].tail(1)
+    print('data: ', newData,  'type: ', type(newData))
+    # return data['mva'][-1]
+
+
 ohlc = kraken.get_ohlc_data({'pair': 'XXBTZUSD', 'interval': 5})['result']['XXBTZUSD']
+
 ohlc_formatted = [
     {
         'Unix Timestamp': data[0],
@@ -16,10 +28,10 @@ ohlc_formatted = [
         'Volume Weighted Average Price': data[5],
         'Volume Traded': data[6],
         'Count': data[7],
-        'Description': 'Bearish' if data[1] > data[4] else 'Bullish'
-    } for data in ohlc]
+        'Description': 'Bearish' if data[1] > data[4] else 'Bullish',
+        'Volume Weighted Moving Average': weighted_moving_average([value[5] for value in ohlc[0: index]],
+                                                                  [value[0] for value in ohlc[0: index]], 5)
+    } for index, data in enumerate(ohlc)]
 
-for data in ohlc_formatted:
-    print(colored(data, 'red') if data['Description'] == 'Bearish' else colored(data, 'green'))
-
-
+# for data in ohlc_formatted:
+#     print(colored(data, 'red') if data['Description'] == 'Bearish' else colored(data, 'green'))
